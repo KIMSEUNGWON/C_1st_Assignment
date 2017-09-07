63b3468b72a01a711bee5243c767f12b07dba865
KIMSEUNGWON <31675723+KIMSEUNGWON@users.noreply.github.com>
2017-09-08 01:49:27 +0900

    C_1st_Assignment language_application2

---
+++ language_application2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+	printf("1. My student id number is 2017301014.\n");
+	printf("2. My name is Kim Seung Won.\n");
+	printf("3. My phone number is 010-8460-7240.\n");
+	printf("4. My GitHub address is hesjs5@naver.com.\n");
+
+
+	return 0;
+	
+}
